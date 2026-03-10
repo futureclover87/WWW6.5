6f040bd441f8c1a37b4771de932d047046529d0a
futureclover87 <sljclover@hotmail.com>
2026-03-10 16:14:21 +0800

    Day09&10 Submission by @futureclover87

--- Lucia/day9_Calculator.sol
+++ Lucia/day9_Calculator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ScientificCalculator.sol";
+import "./day9_ScientificCalculator.sol";
 contract Calculator{
     address public owner;
     address public scientificCalculatorAddress;
